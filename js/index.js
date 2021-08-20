a22ea610fa395752cb181e57ea331348bd794f42
deepanshuJ31 <55781288+deepanshuJ31@users.noreply.github.com>
2021-08-20 20:05:28 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
+/* // Worked on by Deepanshu Joshi
+// Student Id : 8726016
+// Description: The details selected by the user at the Home Page are being redirected to reservations.html page
+
+//-------------------------------------------
+
+*/
 $(document).ready(function(){
 
   var today = new Date().toJSON().slice(0,10).replace(/-/g,'-');
-  $("#checkin").attr("min", today);
+  $("#checkin").attr("min", today);             //setting the min attribute for checkin date 
 
+ // registering click event handler on search button
   $("#search-hotels").click(function(e){
 
     var dateIn = $("#checkin").val();
@@ -10,10 +18,13 @@ $(document).ready(function(){
     var rooms = $("#rooms").val();
     var adults = $("#adults").val();
 
+    //Checks if the value is null
     if(dateIn=="" || dateOut==""){
       e.preventDefault();
       return;
     }
+    
+    // Checks if the Checkin date is higher than checkout date
     if(dateIn > dateOut){
       alert("Checkout date should be higher");
       return;
